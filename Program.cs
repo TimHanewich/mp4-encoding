80c489c49248e0aa31cd3bbd082ed746f8842540
Tim Hanewich <tahanewich@live.com>
2023-03-04 09:28:05 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mp4Encoding
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HI");
+        }
+    }
+}
